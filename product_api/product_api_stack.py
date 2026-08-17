192cb4c3163f7d637d55cb19fa10db1640c70917
AlbertoDuque1006 <albertoduquegamez@gmail.com>
2026-08-17 11:49:57 -0500

    feat: deploy serverless products API with AWS CDK

--- product_api/product_api_stack.py
+++ product_api/product_api_stack.py
@@ -1,19 +1,46 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as lambda_,
+    aws_apigateway as apigw,
 )
+
 from constructs import Construct
+
 
 class ProductApiStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(
+        self,
+        scope: Construct,
+        construct_id: str,
+        **kwargs
+    ):
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Lambda que ejecuta get_product.py
+        get_product = lambda_.Function(
+            self,
+            "GetProductFunction",
+            runtime=lambda_.Runtime.PYTHON_3_12,
+            handler="get_product.handler",
+            code=lambda_.Code.from_asset("lambda_code"),
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ProductApiQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # API Gateway
+        api = apigw.RestApi(
+            self,
+            "ProductsApi",
+            rest_api_name="Products API",
+        )
+
+        # /products
+        products = api.root.add_resource("products")
+
+        # /products/{id}
+        product_by_id = products.add_resource("{id}")
+
+        # GET /products/{id}
+        product_by_id.add_method(
+            "GET",
+            apigw.LambdaIntegration(get_product)
+        )
